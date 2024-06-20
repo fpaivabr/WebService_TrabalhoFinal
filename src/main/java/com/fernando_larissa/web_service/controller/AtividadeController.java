060cad3d4fe1898eafe1b1d77e0c4990ab2387cd
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2024-06-20 17:18:26 -0300

    add /api/atividades

--- src/main/java/com/fernando_larissa/web_service/controller/AtividadeController.java
+++ src/main/java/com/fernando_larissa/web_service/controller/AtividadeController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/atividades")
+@RequestMapping("/api/atividades")
 public class AtividadeController {
 
     @Autowired
